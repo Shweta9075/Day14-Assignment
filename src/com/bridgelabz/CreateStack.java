082b9f970c4b82685dead2b7e75596d42e7ca48c
shweta <shwetabirajdar5872gmail.com>
2022-10-26 13:42:56 +0530

    Added: pop and peek operation

--- src/com/bridgelabz/CreateStack.java
+++ src/com/bridgelabz/CreateStack.java
@@ -5,10 +5,16 @@ import java.util.Stack;
 public class CreateStack {
     public static void main(String[] args) {
         Stack<Integer> stk = new Stack<>();
+        System.out.println("Empty stack: " + stk.isEmpty());
         // pushing elements into Stack
         stk.push(70);
         stk.push(30);
         stk.push(56);
         System.out.println("Stack: " + stk);
+        System.out.println("Empty stack: " + stk.isEmpty());
+        System.out.println("Stack pop: " + stk.pop());
+        System.out.println("Stack peek: " + stk.peek());
+        System.out.println("Empty stack: " + stk.isEmpty());
+
     }
 }
